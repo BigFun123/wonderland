e953d9c9da586e944b540ac7892a4514cc29be30
karl <github@bigfun.co.za>
2026-04-06 19:00:18 +0200

    genie link

--- src/pages/main.js
+++ src/pages/main.js
@@ -13,7 +13,7 @@ function Main(options) {
 
     return (
         <div className='flexbox waitinglist'>
-           <h1 className='shadow'>Play Outside. Play Chess. Chat with Genie. Play Wartorn.</h1>
+           <h1 className='shadow'>Play Outside. Play Chess. <a href="https://genie-g0fmfsdubrb8h8at.southafricanorth-01.azurewebsites.net/">Chat with Genie.</a> Play Wartorn.</h1>
         </div>
     );
 }
